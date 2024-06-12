3ecec636a15fb25ff22a440e52f13a349e2a2a71
Saurabhsaini2002 <127646569+Saurabhsaini2002@users.noreply.github.com>
2024-06-12 19:36:53 +0530

    Add files via upload

---
+++ TNT WEBSITE  PROJECT/index.js
@@ -0,0 +1,58 @@
+// the heading part of the page where text change by itself.
+const words = ["Greetings!", "How Can We Help You...",];
+let i = 0;
+let timer;
+
+function typingEffect() {
+    let word = words[i].split("");
+    var loopTyping = function() {
+        if (word.length > 0) {
+            document.getElementById('word').innerHTML += word.shift();
+        } else {
+            deletingEffect();
+            return false;
+        };
+        timer = setTimeout(loopTyping, 350);
+    };
+    loopTyping();
+};
+
+function deletingEffect() {
+    let word = words[i].split("");
+    var loopDeleting = function() {
+        if (word.length > 0) {
+            word.pop();
+            document.getElementById('word').innerHTML = word.join("");
+        } else {
+            if (words.length > (i + 1)) {
+                i++;
+            } else {
+                i = 0;
+            };
+            typingEffect();
+            return false;
+        };
+        timer = setTimeout(loopDeleting, 200);
+    };
+    loopDeleting();
+};
+
+typingEffect();
+
+
+
+// menu bar
+var MenuItems = document.getElementById("MenuItems");
+    
+    MenuItems.style.maxHeight = "0px";
+
+    function menutoggle(){
+        if(MenuItems.style.maxHeight == "0px")
+        {
+            MenuItems.style.maxHeight = "200px";
+        }
+        else
+        {
+            MenuItems.style.maxHeight = "0px";
+        }
+    } 
